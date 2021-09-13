f07de718d96a2bd3716efa242f2e2e0e3a8b79b7
Aleksandr <sanek.pershin.68rus@yandex.ru>
2021-09-13 13:42:41 +0300

    new commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Лабораторная работа №1 </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Результат вычисления объема треугольной пирамиды с высотой 41 и стороной основания 6 равен ";
+				echo (41*6*6)/(4*3^(1/3);
+			?>
+		</div>
+	</body>
+</html>
